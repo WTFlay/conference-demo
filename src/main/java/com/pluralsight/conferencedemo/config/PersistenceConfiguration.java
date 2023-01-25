4bbf252dec190f2ef2a6a52d21cbb67f12b545df
Flavien Schriever <flavien@schriever.dev>
2023-01-25 18:58:00 +0100

    fix: remove user and password env variable

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 
 @Configuration
 public class PersistenceConfiguration {
@@ -14,19 +13,11 @@ public class PersistenceConfiguration {
     @Value("${db.url}")
     private String url;
 
-    @Value("${db.username}")
-    private String username;
-
-    @Value("${db.password}")
-    private String password;
-
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
                 .url(url)
-                .username(username)
-                .password(password)
                 .build();
     }
 }
