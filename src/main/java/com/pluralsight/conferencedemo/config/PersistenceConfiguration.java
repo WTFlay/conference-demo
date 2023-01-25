af7f1f8d95899b5af21d20b9e516aedc747e1854
Flavien Schriever <flavien@schriever.dev>
2023-01-25 22:24:06 +0100

    build: make application building war

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -2,7 +2,5 @@ package com.pluralsight.conferencedemo.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class PersistenceConfiguration {}
