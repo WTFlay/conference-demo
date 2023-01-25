33315150fecd558d2c48517f3092472f65fde18f
Flavien Schriever <flavien@schriever.dev>
2023-01-25 19:01:54 +0100

    build: empty PersistenceConfiguration

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,23 +1,8 @@
 package com.pluralsight.conferencedemo.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class PersistenceConfiguration {
-
-    @Value("${db.url}")
-    private String url;
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder
-                .create()
-                .url(url)
-                .build();
-    }
-}
+public class PersistenceConfiguration {}
